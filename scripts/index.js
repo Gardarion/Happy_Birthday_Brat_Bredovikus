66a11d5dbd310623d2b1ce4a3fb3d76e42785fc9
Gardarion <72802580+Gardarion@users.noreply.github.com>
2020-10-14 02:17:29 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,24 @@ function enter()
 var code = document.getElementById("my_pass").value;
 var asap = "Gray";
 var pass = "123456";
-var dvas = "ПОДСТ4В4";
+var dvas = "НОРМАЛЬНЫЙ БРАТ";
+var dddd = "НОРМАЛЬНЫЙ БРАТ!";
+var vvvv = "Нормальный брат!";
+var aaaa = "Нормальный брат";
 var kras = "МОМ3НТ";
 var vras = "УБ1ЙСТВО";
 var dras = "ПР4ВОСУД13";
 var asas = "ХОНК";
 var sasa = "БРОСОК";
-var urod = "Пароль неверный, уродиньёс!";
+var urod = "Пароль неверный! Штизебрат?";
 if(code==asap)
 {location.href="coderavenGray.html";}
 else{
 if(code==pass)
 {location.href="atiumenmister.html";}
 else{
-if(code==dvas)
-{location.href="";}
+if(code==dvas || code==dddd || code==vvvv || code==aaaa)
+{location.href="normalniybrat.html";}
 else{
 if(code==kras)
 {location.href="";}
